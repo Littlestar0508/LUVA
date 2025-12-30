a372e3452809131a67cd061747d61dc1e706358f
Littlestar0508 <rist9860@gmail.com>
2025-12-30 11:32:56 +0900

    FEAT : 로그아웃 및 로그인 체크 구현

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,7 +1,20 @@
+import { useEffect } from "react";
 import GoogleLoginButton from "./GoogleLoginButton";
 import KakaoLoginButton from "./KakaoLoginButton";
+import Logout from "./Logout";
+import { supabase } from "../lib/SupabaseClient";
 
 function Login() {
+  const checkLogin = async () => {
+    const { data } = await supabase.auth.getSession();
+
+    console.log(data);
+  };
+
+  useEffect(() => {
+    checkLogin();
+  });
+
   return (
     <>
       <div className="flex flex-col gap-3">
@@ -9,6 +22,7 @@ function Login() {
         <KakaoLoginButton />
 
         <GoogleLoginButton />
+        <Logout />
       </div>
     </>
   );
---
+++ src/components/Logout.tsx
@@ -0,0 +1,16 @@
+import { supabase } from "../lib/SupabaseClient";
+
+function Logout() {
+  const logOut = async () => {
+    const { error } = await supabase.auth.signOut();
+    console.log(error);
+  };
+
+  return (
+    <>
+      <button onClick={logOut}>로그아웃</button>
+    </>
+  );
+}
+
+export default Logout;
