5fde909efb65a04996d4d6e2299795a9491f32c4
Littlestar0508 <rist9860@gmail.com>
2026-01-02 16:42:08 +0900

    FEAT : 간단한 레이아웃 구성

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,10 +2,16 @@ import { useEffect, useState } from "react";
 import { supabase } from "../lib/SupabaseClient";
 import { FaHeart } from "react-icons/fa";
 import { FaBell } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
   const [profileImg, setProfileImg] = useState<string | null>(null);
   const [nickname, setNickname] = useState<string | null>(null);
+  const navigate = useNavigate();
+
+  const moveToMyPage = () => {
+    navigate("/mypage");
+  };
 
   useEffect(() => {
     const getProfileImgURL = async () => {
@@ -30,15 +36,40 @@ function Home() {
   // 임시 홈 페이지
   return (
     <>
-      <div>
-        <img src="../../public/luva-logo-shape.svg" />
-        <img src="../../public/luva-logo-text.svg" className="w-20" />
-        <FaBell />
+      <div className="flex relative justify-center border-b pb-4">
+        <img src="../../public/luva-logo-text.svg" className="w-25" />
+        <button
+          aria-label="알림함"
+          onClick={() => {
+            alert("미구현 상태");
+          }}
+          className="absolute right-0"
+        >
+          <FaBell size={28} />
+        </button>
       </div>
-      <div className="flex flex=col">
-        <img src={profileImg ?? "../../public/basic_profile.png"}></img>
-        <p>{nickname}</p>
-        <FaHeart />
+      <div className="flex flex-col pt-4 items-center gap-2">
+        {/* 프로필 이미지 */}
+        <img
+          src={profileImg ?? "../../public/basic_profile.png"}
+          className="w-80 rounded-2xl"
+        />
+        {/* 닉네임 */}
+        <p className="rounded-2xl text-2xl">{nickname}</p>
+        {/* 좋아요 수와 프로필 편집 버튼 container */}
+        <div className="flex gap-2 w-80">
+          <div className="flex items-center justify-center gap-4 bg-luva-line border-luva-line-soft border-2 rounded-lg p-4 flex-1">
+            <FaHeart size={24} className="fill-luva-like" />
+            {/* 더미 데이터 -> 이후 데이터 fetching예정 */}
+            <p className="text-2xl">10</p>
+          </div>
+          <button
+            className="flex justify-center items-center gap-4 bg-luva-line border-luva-line-soft border-2 rounded-lg p-4 flex-2 text-2xl"
+            onClick={moveToMyPage}
+          >
+            프로필 편집
+          </button>
+        </div>
       </div>
     </>
   );
