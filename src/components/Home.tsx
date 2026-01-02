25162b490e98f996dd83898e1bd09d12c20ef795
Littlestar0508 <rist9860@gmail.com>
2026-01-02 18:20:15 +0900

    MODIFY : 이미지 경로 재재수정...

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -55,11 +55,7 @@ function Home() {
   return (
     <>
       <div className="flex relative justify-center border-b pb-4">
-        <img
-          src="../../public/luva-logo-text.svg"
-          className="w-25"
-          aria-label="LUVA"
-        />
+        <img src="/luva-logo-text.svg" className="w-25" aria-label="LUVA" />
       </div>
       <div className="flex flex-col pt-4 items-center gap-2">
         {/* 프로필 이미지 */}
