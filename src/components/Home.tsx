c5b810ec3c2d8f319a4a00ab3932083a954e6341
Littlestar0508 <rist9860@gmail.com>
2026-01-02 15:22:57 +0900

    FEAT : 프로필 이미지와 닉네임 렌더링

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,17 +1,42 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { supabase } from "../lib/SupabaseClient";
+import { FaHeart } from "react-icons/fa";
 
 function Home() {
+  const [profileImg, setProfileImg] = useState<string | null>(null);
+  const [nickname, setNickname] = useState<string | null>(null);
+
   useEffect(() => {
-    supabase.auth.getSession().then(({ data }) => {
-      console.log(data);
-    });
+    const getProfileImgURL = async () => {
+      const { data, error } = await supabase.auth.getSession();
+
+      if (error) throw error;
+
+      const user = data.session?.user;
+      const user_metadata = user?.user_metadata ?? {};
+
+      const profileImgURL =
+        user_metadata.avatar_url ?? user_metadata.picture ?? null;
+
+      console.log(user_metadata);
+      setProfileImg(profileImgURL);
+      setNickname(user_metadata.full_name);
+    };
+
+    getProfileImgURL();
   });
 
   // 임시 홈 페이지
   return (
     <>
-      <div className="bg-luva-like">홈페이지 영역입니다.</div>
+      <div>
+        <img></img>
+      </div>
+      <div className="flex flex=col">
+        <img src={profileImg ?? "../../public/basic_profile.png"}></img>
+        <p>{nickname}</p>
+        <FaHeart />
+      </div>
     </>
   );
 }
