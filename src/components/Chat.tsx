96d83bd73e47301941c893bcbd73caa63b3ca211
Littlestar0508 <rist9860@gmail.com>
2026-01-09 10:05:52 +0900

    FEAT : 채팅방 리스트 불러오기 구현

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -3,39 +3,45 @@ import { supabase } from "../utils/SupabaseClient";
 import useUserProfileStore from "../utils/UserProfileStore";
 
 type ChatRoomList = {
-  id: string;
-  user_a: string;
-  user_b: string;
-  created_at: string;
+  last_message: string;
+  last_message_at: string;
+  partner_id: string;
+  partner_profile_img: string;
+  partner_nickname: string;
+  room_id: string;
 };
 
 function Chat() {
-  const { id } = useUserProfileStore();
   const [chatList, setChatList] = useState<ChatRoomList[]>([]);
 
   useEffect(() => {
-    const fetchChatRooms = async () => {
-      const { data, error } = await supabase
-        .from("chat_room")
-        .select("id, user_a, user_b, created_at")
-        .or(`user_a.eq.${id},user_b.eq.${id}`);
+    const fetchChatRoomsList = async () => {
+      const { data, error } = await supabase.rpc("get_chat_room_list");
 
-      if (error) {
-        console.error(error);
-        return;
-      }
-
-      setChatList(data ?? []);
+      setChatList(data);
     };
 
-    fetchChatRooms();
-  }, [id]);
+    fetchChatRoomsList();
+  }, []);
 
   return (
     <>
       <div className="bg-luva-line">채팅창 영역입니다.</div>
       {chatList.map((m) => (
-        <div>{m.id}</div>
+        <div className="flex items-center gap-3" key={m.room_id} id={m.room_id}>
+          <img
+            src={m.partner_profile_img}
+            alt={m.partner_nickname + "프로필 이미지"}
+            className="size-12 rounded-xl shrink-0"
+          />
+          <div className="flex flex-col gap-2 flex-1">
+            <p className="text-sm text-luva-text-strong font-bold">
+              {m.partner_nickname}
+            </p>
+            <p className="text-xs">{m.last_message}</p>
+          </div>
+          <p className="text-xs">{m.last_message_at}</p>
+        </div>
       ))}
     </>
   );
