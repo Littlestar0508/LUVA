8e443330915972c3fcc687e8a7e4052dd318d9e0
Littlestar0508 <rist9860@gmail.com>
2026-01-09 14:06:29 +0900

    FEAT : 클릭 시 채팅 방 이동

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { supabase } from "../utils/SupabaseClient";
 import TimeFormatter from "../utils/TimeFormatter";
+import { useNavigate } from "react-router-dom";
 
 type ChatRoomList = {
   last_message: string;
@@ -12,7 +13,12 @@ type ChatRoomList = {
 };
 
 function Chat() {
+  const navigate = useNavigate();
   const [chatList, setChatList] = useState<ChatRoomList[]>([]);
+
+  const moveToChatRoom = (params: string) => {
+    navigate(`/chat-content?id=${params}`);
+  };
 
   // 채팅방 목록 불러오기
   useEffect(() => {
@@ -38,7 +44,7 @@ function Chat() {
           schema: "public",
           table: "chat_messages",
         },
-        async (payload) => {
+        async () => {
           const { data } = await supabase.rpc("get_chat_room_list");
           if (data) setChatList(data);
         }
@@ -55,7 +61,7 @@ function Chat() {
           schema: "public",
           table: "chat_room",
         },
-        async (payload) => {
+        async () => {
           const { data } = await supabase.rpc("get_chat_room_list");
           if (data) setChatList(data);
         }
@@ -71,20 +77,42 @@ function Chat() {
   return (
     <>
       {chatList.map((m) => (
-        <div className="flex items-center gap-3" key={m.room_id} id={m.room_id}>
-          <img
-            src={m.partner_profile_img}
-            alt={m.partner_nickname + "프로필 이미지"}
-            className="size-12 rounded-xl shrink-0"
-          />
-          <div className="flex flex-col gap-2 flex-1">
-            <p className="text-sm text-luva-text-strong font-bold">
-              {m.partner_nickname}
-            </p>
-            <p className="text-xs">{m.last_message}</p>
-          </div>
-          <p className="text-xs">{TimeFormatter(m.last_message_at)}</p>
-        </div>
+        <li
+          key={m.room_id}
+          className="list-none active:bg-luva-chat text-luva-text-strong active:text-black p-3"
+        >
+          <button
+            type="button"
+            className="flex items-center gap-3 w-full text-left"
+            aria-label={`${m.partner_nickname} 님과의 채팅방 열기`}
+            onClick={() => moveToChatRoom(m.room_id)}
+          >
+            {/* 프로필 이미지 */}
+            <figure className="shrink-0">
+              <img
+                src={m.partner_profile_img}
+                alt={`${m.partner_nickname}의 프로필 이미지`}
+                className="size-12 rounded-xl border border-luva-line"
+              />
+            </figure>
+
+            {/* 채팅 정보 */}
+            <article className="flex flex-col gap-2 flex-1 overflow-hidden">
+              <header>
+                <p className="text-sm font-bold truncate w-1/2">
+                  {m.partner_nickname}
+                </p>
+              </header>
+
+              <p className="text-xs truncate w-4/5">{m.last_message}</p>
+            </article>
+
+            {/* 시간 */}
+            <time dateTime={m.last_message_at} className="text-xs shrink-0">
+              {TimeFormatter(m.last_message_at)}
+            </time>
+          </button>
+        </li>
       ))}
     </>
   );
