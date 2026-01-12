84b89655c88119a1aaa9cf6936940667514c70d0
Littlestar0508 <rist9860@gmail.com>
2026-01-12 10:00:26 +0900

    FEAT : 스와이퍼를 이용한 프로필 이미지 렌더링

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,15 +1,27 @@
 import SearchProfileBase from "./SearchProfileBase";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
 
 function Search() {
+  const textMap = Array(10).fill(0);
+
   return (
-    <SearchProfileBase
-      src={"/basic_profile.png"}
-      alt="프로필 사진"
-      hobby={"취미"}
-      place={"사는 곳"}
-      id={"123"}
-      name={"닉네임"}
-    />
+    <>
+      <Swiper slidesPerView={1} className="h-full">
+        {textMap.map(() => (
+          <SwiperSlide>
+            <SearchProfileBase
+              src={"/basic_profile.png"}
+              alt="프로필 사진"
+              hobby={"취미"}
+              place={"사는 곳"}
+              id={"123"}
+              name={"닉네임"}
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </>
   );
 }
 
