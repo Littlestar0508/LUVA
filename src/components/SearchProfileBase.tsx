0a98e062d7135767444961dc91df38004499f88d
Littlestar0508 <rist9860@gmail.com>
2026-01-12 16:48:58 +0900

    FEAT : 채팅 생성 및 이동

--- src/components/SearchProfileBase.tsx
+++ src/components/SearchProfileBase.tsx
@@ -1,3 +1,8 @@
+import { useState } from "react";
+import useUserProfileStore from "../utils/UserProfileStore";
+import { supabase } from "../utils/SupabaseClient";
+import { useNavigate } from "react-router-dom";
+
 type SearchProfileBaseProps = {
   id: string;
   src: string;
@@ -15,12 +20,53 @@ function SearchProfileBase({
   place,
   hobby,
 }: SearchProfileBaseProps) {
-  const openChatRoom = () => {
-    console.log("채팅열기");
+  const navigate = useNavigate();
+  const [liked, setLiked] = useState(false);
+
+  const { id: myId } = useUserProfileStore();
+
+  const openChatRoom = async () => {
+    const orderUUID = [];
+    if (myId < id) {
+      orderUUID[0] = myId;
+      orderUUID[1] = id;
+    } else {
+      orderUUID[0] = id;
+      orderUUID[1] = myId;
+    }
+
+    const { data: chkHasRoom } = await supabase
+      .from("chat_room")
+      .select("*")
+      .eq("user_a", orderUUID[0])
+      .eq("user_b", orderUUID[1])
+      .maybeSingle();
+
+    if (chkHasRoom) {
+      navigate(`/chat-content?id=${chkHasRoom.id}`);
+    }
+
+    if (!chkHasRoom) {
+      const { data, error } = await supabase
+        .from("chat_room")
+        .insert([{ user_a: orderUUID[0], user_b: orderUUID[1] }])
+        .select();
+
+      if (error) {
+        console.error(error);
+        return;
+      }
+
+      navigate(`/chat-content?id=${data[0].id}`);
+    }
   };
 
   const pressLike = () => {
     console.log("좋아요");
+
+    const nextLiked = !liked;
+
+    setLiked(nextLiked);
   };
 
   return (
@@ -45,13 +91,15 @@ function SearchProfileBase({
         </figure>
         <button
           onClick={openChatRoom}
-          className="col-span-2 row-span-2 bg-luva-chat text-luva-bg-0 p-1 rounded-lg h-full"
+          className="border border-luva-chat col-span-2 row-span-2 bg-luva-chat text-luva-bg-0 p-1 rounded-lg h-full"
         >
           채팅열기
         </button>
         <button
           onClick={pressLike}
-          className="col-span-2 row-span-2 bg-luva-like text-luva-text-strong p-1 rounded-lg h-full"
+          className={`border border-luva-like col-span-2 row-span-2 text-luva-text-strong p-1 rounded-lg h-full ${
+            liked ? "bg-luva-like" : null
+          }`}
         >
           좋아요
         </button>
